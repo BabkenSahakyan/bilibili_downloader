b1db2c5ccefd2e294c6f081c3e0a53e9ceb53ba0
BabkenSahakyan <lienosayne@gmail.com>
2024-11-06 20:22:23 +0400

    using json5

--- downloader.py
+++ downloader.py
@@ -1,12 +1,12 @@
-import json
+import json5
 import urllib.request
 import csv
 import os
 
 
-def read_url(quality, ext):
+def read_url(name, quality, ext):
     result = []
-    with open("csv_output.csv", "r") as urls_file:
+    with open(name + ".csv", "r") as urls_file:
         lines = csv.reader(urls_file, delimiter='|')
         for line in lines:
             if line[0] == quality and line[1] == ext:
@@ -16,13 +16,13 @@ def read_url(quality, ext):
 
 
 if __name__ == '__main__':
-    conf_file = open("conf.json", "r")
-    conf = json.loads(conf_file.read())
+    conf_file = open("conf.json5", "r")
+    conf = json5.loads(conf_file.read())
     conf_file.close()
 
-    folder = conf["name"]
-    os.makedirs(folder, exist_ok=True)
+    name = conf["name"]
+    os.makedirs(name, exist_ok=True)
 
-    for title, url in read_url(conf["preferred_quality"], conf["preferred_ext"]):
+    for title, url in read_url(name, conf["preferred_quality"], conf["preferred_ext"]):
         print(title + " " + url)
-        urllib.request.urlretrieve(url, folder + "/" + title)
+        urllib.request.urlretrieve(url, name + "/" + title)
